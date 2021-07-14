7c9cd02a99c7f63e1eb20a891084d9dacc86165e
alexander <AMKovalenko@outlook.com>
2021-07-14 12:55:37 +0300

    less2_using templates

--- flsite.py
+++ flsite.py
@@ -1,17 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+menu = ["установка", "первое приложение", "Обратная связь"]
 
 
 @app.route("/")
 @app.route("/index")
 def index():
-    return "index"
+    return render_template("index.html", menu=menu)
 
 
 @app.route("/about")
 def about():
-    return "<h1>О сайте<h1>"
+    return render_template("about.html", title="О сайте")
 
 
 if __name__ == "__main__":
