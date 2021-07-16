bb3d0bedaaf39ceb44af453a45810fc08bf40f0c
alexander <AMKovalenko@outlook.com>
2021-07-16 14:12:33 +0300

    less6_Flash_msg

--- flsite.py
+++ flsite.py
@@ -1,19 +1,43 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request, flash
 
 app = Flask(__name__)
-
-menu = ["установка", "первое приложение", "Обратная связь"]
+app.config['SECRET_KEY'] = 'sasfdasdzxadfadsf'
+# menu = ["установка", "первое приложение", "Обратная связь"]
+menu = [{"name": "Установка", "url": "install-flask"},
+        {"name": "Первое приложение", "url": "first-app"},
+        {"name": "Обратная связь", "url": "contact"},
+        ]
 
 
 @app.route("/")
-@app.route("/index")
 def index():
+    print(url_for('index'))
     return render_template("index.html", menu=menu)
 
 
 @app.route("/about")
 def about():
+    print(url_for('about'))
     return render_template("about.html", title="О сайте")
+
+
+@app.route("/profile/<int:username>")
+def profile(username):
+    return f"Пользователь : {username}"
+
+
+@app.route("/contact", methods=["POST", "GET"])
+def contact():
+    if request.method == "POST":
+        print(request.form)
+        print(request.form['username'])
+
+        if len(request.form['username']) > 2:
+            flash('Сообщение отправлено', category='success')
+        else:
+            flash("Ошибка отправки", category='error')
+
+    return render_template('contact.html', title="Обратная связь", menu=menu)
 
 
 if __name__ == "__main__":
